e250e37ad6684d7d3b5597b1d7ee7d5b905275f7
herotanhoa3 <huynhtanhoa2012@gmail.com>
2016-09-19 16:44:54 +0700

    updated

---
+++ cdstore.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 19, 2016 at 11:26 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `cdstore`
+--
+CREATE DATABASE IF NOT EXISTS `cdstore` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `cdstore`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cd`
+--
+
+CREATE TABLE IF NOT EXISTS `cd` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `singer` varchar(150) NOT NULL,
+  `numbersongs` int(11) NOT NULL,
+  `price` double NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `cd`
+--
+
+INSERT INTO `cd` (`id`, `name`, `singer`, `numbersongs`, `price`) VALUES
+(1, 'You will never walk alone', 'Steven Gerrard', 3, 100000),
+(2, 'Beautiful in white', 'Beatle', 5, 120000);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
